000324236c45ccc76db108b3d189bd50aab1fe34
ihalilovic1 <irhadhalilovic@hotmail.com>
2018-12-18 21:39:32 +0100

    maskirane slike

--- Skripte/fileScripts.py
+++ Skripte/fileScripts.py
@@ -1,4 +1,10 @@
 from skimage import io
+import numpy as np
+
+def saveImage(image, location, rot, flip):  
+    io.imsave(location, np.flip(np.rot90(image, rot), flip))
+    #np.rot90(image, 3)
 
 def saveImage(image, location):  
     io.imsave(location, image)
+    #np.rot90(image, 3)
--- Skripte/mask.py
+++ Skripte/mask.py
@@ -3,30 +3,60 @@ import pandas as pd
 import json
 import warnings
 from skimage.draw import polygon
+from skimage import io
 from indexOfSubstring import indexOfSubstring
 import fileScripts as fs
+import matplotlib.pyplot as plt
+import os
+from PIL import Image
+from PIL import  ImageChops
 
 warnings.filterwarnings('ignore')
 
-regionsOfInterestJSON = open('/home/irhad/Desktop/POOS/Dataset/RegionsOfInterest/Sign-L.json')
-annotationStrings = regionsOfInterestJSON.readlines()
+def createMasks():
 
-for jsonString in annotationStrings:
-    jsonData = json.loads(jsonString)
-    maskPath = indexOfSubstring(jsonData['content'], 'IMG')
+    regionsOfInterestJSON = open('/home/irhad/Desktop/POOS/Dataset/RegionsOfInterest/Sign-L.json')
+    annotationStrings = regionsOfInterestJSON.readlines()
 
-    mask = np.zeros([100,100,3], dtype=int)
-    points = jsonData['annotation'][0]['points']
-    poly = []
-    for point in points:
-        poly.append((int(point[0] * 100), int(point[1] * 100)))
-    poly = np.array(poly)
-    rr, cc = polygon(poly[:,0], poly[:,1], mask.shape)
-    mask[rr,cc,:] = (255, 255, 255)
+    for jsonString in annotationStrings:
+        jsonData = json.loads(jsonString)
+        maskPath = indexOfSubstring(jsonData['content'], 'IMG')
 
-    fs.saveImage(mask[:, :, :], '/home/irhad/Desktop/POOS/Dataset/Masks/' + maskPath)
+        mask = np.zeros([100,100,3], dtype=int)
+        points = jsonData['annotation'][0]['points']
+        poly = []
+        for point in points:
+            poly.append((int(point[0] * 100), int(point[1] * 100)))
+        poly = np.array(poly)
+        rr, cc = polygon(poly[:,0], poly[:,1], mask.shape)
+        mask[rr,cc,:] = (255, 255, 255)
 
+        fs.saveImage(mask[:, :, :], '/home/irhad/Desktop/POOS/Dataset/Masks/' + maskPath, 1, (0, 2))
+
+def maskImages():
+    maskPath = '/home/irhad/Desktop/POOS/Dataset/Masks/'
+    originalImagePath = '/home/irhad/Desktop/POOS/Dataset/Images/'
+    maskedImagePath = '/home/irhad/Desktop/POOS/Dataset/MaskedImages/'
+
+    for i in range(0,10):
+        for filename in os.listdir(originalImagePath + str(i)):
+            #mask = io.imread(maskPath + str(i) + '/' + filename)
+            #image = io.imread(originalImagePath + str(i) + '/' + filename)
+
+            mask = Image.open(maskPath + str(i) + '/' + filename)
+            image = Image.open(originalImagePath + str(i) + '/' + filename)
+            image.load()
+            mask.load()
+            result = ImageChops.multiply(image,mask)
+            result = np.asarray(result, dtype="int32")
+            
+            fs.saveImage(result[:, :, :], maskedImagePath + str(i) + '/' + filename)
+
+            #plt.imshow(result)
+            #plt.show()     
+
+#createMasks()
+maskImages()
     #plt.imshow(mask)
     #break
 #plt.show()
-#def maskImage(originalPath, maskPath):
